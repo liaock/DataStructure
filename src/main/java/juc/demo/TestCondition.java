e1a10987890c2c67afba526aceefde8f38c7f12f
liaock <307860278@qq.com>
2021-12-30 14:37:21 +0800

    init

--- src/main/java/juc/demo/TestCondition.java
+++ src/main/java/juc/demo/TestCondition.java
@@ -1,7 +1,79 @@
 package juc.demo;
 
+import java.util.LinkedList;
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
 /**
+ * 生产者消费者问题，用Condtion实现
  * Created by dell on 2021/12/22.
  */
-public class TestCondition {
+public class TestCondition<T> {
+    private final LinkedList<T> lists = new LinkedList<T>();
+    private final int MAX = 10;  // 最多10个元素.
+    private int count = 0;
+
+    Lock lock = new ReentrantLock();
+    Condition consumer = lock.newCondition();
+    Condition producer = lock.newCondition();
+
+    public void put(T t){
+        lock.lock();
+        try {
+            while(lists.size() == MAX){
+                try {
+                    producer.await();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+            lists.add(t);
+            ++ count;
+            consumer.signalAll();  // 通知消费者消费
+        } finally {
+            lock.unlock();
+        }
+    }
+
+    public T get(){
+        T t;
+        try {
+            lock.lock();
+            while (lists.size()==0){
+                try {
+                    consumer.await();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+            t = lists.removeFirst();
+            count --;
+            producer.signalAll();
+        } finally {
+            lock.unlock();
+        }
+        return t;
+    }
+
+
+    public static void main(String[] args) {
+        TestCondition testCondition = new TestCondition();
+        //启动消费线程
+        for (int i = 0; i < 100; i++) {
+            new Thread(()->{
+                System.out.println("消费元素：" + testCondition.get());
+            }).start();
+        }
+
+        //启动生产线程
+        for (int i = 0; i < 100; i++) {
+            final int k = i;
+            new Thread(()->{
+                System.out.println("生产元素:"+k);
+                testCondition.put( k);
+            }).start();
+        }
+    }
+
 }
--- src/main/java/juc/demo/TestSemaphore.java
+++ src/main/java/juc/demo/TestSemaphore.java
@@ -1,7 +1,59 @@
 package juc.demo;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.Semaphore;
+import java.util.concurrent.TimeUnit;
+
 /**
- * Created by dell on 2021/12/22.
+  * 实现一个容器，提供两个方法，add, size
+  * 写两个线程，线程1添加10个元素到容器中，线程2实现监控元素个数，当个数到5个时，线程2给出提示并结束
+  * Created by dell on 2021/12/22.
  */
 public class TestSemaphore {
+
+    List lists = new ArrayList();
+    public void add(Object o){lists.add(o);}
+    public int size(){return lists.size();}
+
+    public static void main(String[] args) {
+        TestSemaphore testSemaphore = new TestSemaphore();
+        Semaphore semaphore = new Semaphore(1);
+
+        Thread t1 = new Thread(()->{
+            try {
+                semaphore.acquire();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            System.out.println("t1启动");
+            for (int i = 0; i < 10; i++) {
+                testSemaphore.add(i);
+                System.out.println("add " + i);
+                if (testSemaphore.size() == 5) {
+                    semaphore.release();
+                }
+                try {
+                    TimeUnit.SECONDS.sleep(1);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
+        Thread t2 = new Thread(()-> {
+                System.out.println("t2启动");
+            try {
+                semaphore.acquire();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            System.out.println("t2 执行结束");
+            semaphore.release();
+        });
+        t1.start();
+        t2.start();
+
+    }
+
 }
