0f48082a02449a3beb01e5c08ee69cb8b924a81e
liaock <307860278@qq.com>
2022-01-07 11:31:16 +0800

    init

---
+++ src/main/java/practice/leetCode/top200/两数相加.java
@@ -0,0 +1,28 @@
+package practice.leetCode.top200;
+/**
+ * 给你两个 非空 的链表，表示两个非负的整数。它们每位数字都是按照 逆序 的方式存储的，并且每个节点只能存储 一位 数字。
+ *
+ * 请你将两个数相加，并以相同形式返回一个表示和的链表。
+ *
+ * 你可以假设除了数字 0 之外，这两个数都不会以 0 开头。
+ *
+ 输入：l1 = [2,4,3], l2 = [5,6,4]
+ 输出：[7,0,8]
+ 解释：342 + 465 = 807.
+
+ * Created by liaock on 2022/1/7
+ **/
+
+public class 两数相加 {
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        return null;
+    }
+
+    public class ListNode {
+      int val;
+      ListNode next;
+      ListNode() {}
+      ListNode(int val) { this.val = val; }
+      ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+  }
+}
